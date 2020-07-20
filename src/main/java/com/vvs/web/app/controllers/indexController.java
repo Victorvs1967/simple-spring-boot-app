56e8eace74d7a3117808ee502e575a323c1ad986
Victor <victorsmirnov@me.com>
2020-07-20 13:12:35 +0300

    add user information

--- src/main/java/com/vvs/web/app/controllers/indexController.java
+++ src/main/java/com/vvs/web/app/controllers/indexController.java
@@ -1,18 +1,30 @@
 package com.vvs.web.app.controllers;
 
+import java.security.Principal;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.vvs.web.app.entities.Member;
+import com.vvs.web.app.repositories.MemberRepository;
+
 @Controller
 public class indexController {
 	
+	@Autowired
+	MemberRepository memberRepository;
+	
 	@GetMapping("/")
-	public String viewIndex(Model model) {
-		model.addAttribute("message", "Hello everyone, we are go to Spring Web App together.");
-		model.addAttribute("date", new Date());
+	public String viewIndex(Model model, Principal principal, Member member) {
+
+        if (principal != null) {
+            model.addAttribute("message", "Hello everyone, we are go to back to Spring with together");
+            model.addAttribute("date", new Date());
+            model.addAttribute("members", memberRepository.getOne(principal.getName()));
+        }
 		return "index";
 	}
 }
